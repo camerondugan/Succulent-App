002db3197485e2c43260482c3b5530917d07f9e6
Cameron Dugan <cameron.dugan@protonmail.com>
2021-09-29 20:49:44 -0400

    added shop and fixed icon bugs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-import 'package:succ/plant_card.dart';
-import 'package:succ/shop.dart';
+import 'dart:math';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:window_size/window_size.dart';
 import 'package:card_swiper/card_swiper.dart';
@@ -26,7 +26,6 @@ void setWindowSize() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,25 +34,39 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(), // standard dark theme
       themeMode: ThemeMode.system, // device controls theme
-      home: const MyHomePage(title: 'Succulents'),
+      home: const Succ(title: 'Succulents'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
+class Succ extends StatefulWidget {
+  const Succ({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Succ> createState() => _SuccState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _SuccState extends State<Succ> {
   var plants = [
+    "assets/NoPlant.png",
+    "assets/NoPlant.png",
+    "assets/NoPlant.png",
+    "assets/NoPlant.png",
+  ];
+  var plantWater = [3, 3, 3, 3];
+  var plantWaterExpressions = [
+    "I'm dehydrated",
+    "I'm satisfied",
+    "I'm drowning",
+    "Empty",
+  ];
+  var shopPlants = [
     "assets/Plant #1 Dead.png",
-    "assets/Plant #1 Sprout.png",
+    "assets/Plant #1 Full.png",
     "assets/Plant #1 Medium.png",
+    "assets/Plant #1 Sprout.png",
+    "assets/Plant #1 Full.png",
     "assets/Plant #1 Full.png",
   ];
   var colors = [
@@ -64,42 +77,207 @@ class _MyHomePageState extends State<MyHomePage> {
     0xffA5A58D,
     0xff6B705C
   ];
-  int _index = 0;
+  int pageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  void takeFromShop(index) {
+    int i = 0;
+    for (String s in plants) {
+      if (s == "assets/NoPlant.png") {
+        plants[i] = shopPlants[index];
+        shopPlants[index] = "assets/NoPlant.png";
+        plantWater[i] = 0;
+        break;
+      }
+      i++;
+    }
+    setState(() {});
+  }
+
+  int takeFromHome(index) {
+    int i = 0;
+    for (String plant in shopPlants) {
+      if (plant == 'assets/NoPlant.png') {
+        shopPlants[i] = plants[index];
+        plants[index] = 'assets/NoPlant.png';
+        plantWater[index] = 3;
+        setState(() {});
+        return i;
+      }
+      i++;
+    }
+    return -1;
+  }
+
+  Widget plantCard(String description, double cardHeight, int tagHeight,
+      String plant, double edgeRadius, bool purchasable) {
+    return Container(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(edgeRadius),
+          topRight: Radius.circular(edgeRadius),
+          bottomLeft: Radius.circular(edgeRadius),
+          bottomRight: Radius.circular(edgeRadius),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.75),
+            offset: const Offset(4, 8),
+          ),
+        ],
+        color: Colors.transparent,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(edgeRadius),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              flex: 10,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    "assets/plantbg.jpg",
+                    height: cardHeight,
+                    width: cardHeight,
+                    fit: BoxFit.cover,
+                  ),
+                  Image.asset(
+                    plant,
+                    height: cardHeight,
+                    fit: BoxFit.fill,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: tagHeight,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Text(
+                    description,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget shop() {
+    var cardHeight = MediaQuery.of(context).size.height;
+    var tagHeight = 2;
+    var tags = [];
+    for (String plant in shopPlants) {
+      if (plant == "assets/NoPlant.png") {
+        tags.add("Sold!");
+      } else {
+        tags.add("Buy Me!");
+      }
+    }
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 35, 10, 10),
+      child: GridView.builder(
+        itemCount: shopPlants.length,
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250,
+          childAspectRatio: 1 / 1,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                takeFromShop(index);
+              },
+              child: plantCard(tags[index], cardHeight, tagHeight,
+                  shopPlants[index], 30, true),
+            ),
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    var cardHeight = MediaQuery.of(context).size.width * 7 / 8;
-    var tagHeight = 100;
+    var cardHeight = MediaQuery.of(context).size.width * 8 / 8;
+    var tagHeight = 2;
     var _pages = <Widget>[
       Swiper(
-        itemCount: 4,
+        itemCount: plants.length,
         layout: SwiperLayout.STACK,
         itemWidth: MediaQuery.of(context).size.width * 7 / 8,
         itemHeight: cardHeight + tagHeight,
+        duration: 250,
         itemBuilder: (BuildContext context, int index) {
-          return PlantCardGen(
-              cardHeight: cardHeight,
-              tagHeight: tagHeight,
-              plants: plants,
-              index: index);
+          return GestureDetector(
+            onTap: () {
+              if (plants[index] != "assets/NoPlant.png") {
+                plantWater[index] = min(plantWater[index] + 1, 2);
+              }
+              setState(() {});
+            },
+            onLongPress: () {
+              int hydration = plantWater[index];
+              int shopItemIndex = takeFromHome(index);
+              final snack = SnackBar(
+                content: const Text("You sold your plant! :)"),
+                shape: const RoundedRectangleBorder(),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {
+                    if (shopItemIndex != -1) {
+                      takeFromShop(shopItemIndex);
+                      plantWater[index] = hydration;
+                    }
+                  },
+                ),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snack);
+            },
+            child: plantCard(plantWaterExpressions[plantWater[index]],
+                cardHeight, tagHeight, plants[index], 50, true),
+          );
         },
       ),
-      const ShopPage(),
+      shop(),
     ];
     return Scaffold(
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.blueGrey,
-        index: _index,
+        index: pageIndex,
         color: Colors.black,
-        animationCurve: Curves.fastLinearToSlowEaseIn,
-        animationDuration: const Duration(milliseconds: 1000),
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: const Duration(milliseconds: 300),
         items: const <Widget>[
-          Icon(Icons.list_alt, size: 30),
-          Icon(Icons.store_outlined, size: 30),
+          Icon(Icons.house_rounded, size: 30, color: Colors.white),
+          Icon(Icons.storefront_rounded, size: 30, color: Colors.white),
         ],
         onTap: (index) {
-          setState(() => _index = index);
-          stderr.writeln(_index);
+          setState(() => pageIndex = index);
         },
       ),
       resizeToAvoidBottomInset: false,
@@ -112,7 +290,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       backgroundColor: Colors.blueGrey,
       body: IndexedStack(
-        index: _index,
+        index: pageIndex,
         children: _pages,
       ),
     );
